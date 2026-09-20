a4fe65357ef0a18661216000aca63ec177e91980
Nimer Rodriguez <nrodriguezu@fi.uba.ar>
2026-09-19 21:36:24 -0300

    Se configura conexion a bd

---
+++ app/db.py
@@ -0,0 +1,22 @@
+from flask import g, current_app
+import mysql.connector
+
+def get_db():
+    if "db" not in g:
+        config = current_app.config["APP_CONFIG"]
+        g.db = mysql.connector.connect(
+            host=config.DB_HOST,
+            port=config.DB_PORT,
+            user=config.DB_USER,
+            password=config.DB_PASSWORD,
+            database=config.DB_NAME,
+        )
+    return g.db
+
+def close_db(exception=None):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
+def init_app():
+    app.teardown_appcontext(close_db)
