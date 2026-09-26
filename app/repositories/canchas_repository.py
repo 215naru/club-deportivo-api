725e657f0fc9773773e7769cc585169e519d2cea
Nimer Abel Rodriguez Uro <nimer@Nimers-MacBook-Air.local>
2026-09-26 12:19:29 -0300

    Se genera estructura de CRUD para canchas y se implementa delete

--- app/repositories/canchas_repository.py
+++ app/repositories/canchas_repository.py
@@ -6,3 +6,19 @@ def _fila_cancha(fila):
     fila["techada"] = bool(fila["techada"])
     fila["activa"] = bool(fila["activa"])
     return fila
+
+def insert():
+    pass
+
+def find_all():
+    pass
+
+def update():
+    pass
+
+def delete(id_cancha):
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM canchas WEHRE id=%s",(id_cancha))
+    db.commit()
+    cursor.close()
