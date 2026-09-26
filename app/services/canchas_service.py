34afc2eea18da9c605865090bc873b8d13ed33c2
Nimer Abel Rodriguez Uro <nimer@Nimers-MacBook-Air.local>
2026-09-26 17:31:29 -0300

    Se genera template de service para canchas

---
+++ app/services/canchas_service.py
@@ -0,0 +1,13 @@
+from app.repositories import canchas_repository
+from app.errors import ApiError
+
+def crear_cancha():
+    pass
+def listar_canchas():
+    pass
+def obtener_cancha():
+    pass
+def actualizar_cancha():
+    pass
+def eliminar_cancha():
+    pass
