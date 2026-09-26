232640063fd594634139f8986858f82ea7840225
Nimer Abel Rodriguez Uro <nimer@Nimers-MacBook-Air.local>
2026-09-26 12:56:30 -0300

    Se agrega insercion a las canchas

--- app/repositories/canchas_repository.py
+++ app/repositories/canchas_repository.py
@@ -7,8 +7,16 @@ def _fila_cancha(fila):
     fila["activa"] = bool(fila["activa"])
     return fila
 
-def insert():
-    pass
+def insert(nombre,id_deporte,precio_hora,techada,activa):
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute(
+        "INSERT INTO canchas (nombre,id_deporte,precio_hora,techada,activa) VALUES (%s,%s,%s,%s,%s)",
+        (nombre,id_deporte,precio_hora,techada,activa))
+    db.commit()
+    new_id = cursor.lastrowid
+    cursor.close()
+    return new_id
 
 def find_all():
     pass
